7dce4918da9a496b090497bc2725fb63a3051f18
Vu Huy Khanh <k002259@pc-002259-3.local>
2022-07-08 10:54:49 +0700

    Update script

--- tools/test-result-manage/src/test-execution-handler.ts
+++ tools/test-result-manage/src/test-execution-handler.ts
@@ -1,32 +1,48 @@
-import { Button, Notification, Spinner } from 'kintone-ui-component';
+import { Button, Notification, Spinner, Dropdown } from 'kintone-ui-component';
 import { FIELD_CODES, TEST_MANAGEMENT_APP } from './config';
 import { triggerPluginTestWorkflow } from './github-handler';
 import { Octokit } from '@octokit/core';
 import { updateTestManagementRecord } from './test-result-handler';
 
+kintone.events.on(['app.record.index.show'], async (event: any) => {
+  const octokit = new Octokit({
+    auth: process.env.PAT || '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'
+  });
+  await handleIndexTestExecution(octokit, event);
+});
+
 kintone.events.on(['app.record.detail.show'], async (event: any) => {
   const octokit = new Octokit({
-    auth: process.env.PAT || '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'
+    auth: process.env.PAT || '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'
   });
   await handleTestExecution(octokit, event);
 });
 
-const handleTestExecution = async (octokit: Octokit, event: any) => {
-  const btnRunTestElm = document.getElementById(`user-js-${FIELD_CODES.btnCustomRun}`);
+const handleIndexTestExecution = async (octokit: Octokit, event: any) => {
+  const headerSpace = kintone.app.getHeaderMenuSpaceElement();
   const btnRunTest = new Button({ text: 'Run test', type: 'submit' });
-  if (event.record.ddlTestStatus.value === 'running') {
-    btnRunTest.disabled = true;
-  }
+  const ddlPlugin = new Dropdown({
+    label: 'Plugin',
+    items: [
+      {
+        label: 'multitab',
+        value: 'multiTab'
+      },
+      {
+        label: 'conditional',
+        value: 'conditional'
+      }
+    ],
+  });
 
   btnRunTest.addEventListener('click', async () => {
     const spinner = new Spinner({ text: 'Trigger test' });
     spinner.open();
     try {
-      const selectedPlugin = event.record.ddlPluginName.value;
+      const selectedPlugin = ddlPlugin.value;
       await triggerPluginTestWorkflow(octokit, selectedPlugin);
       await updateTestManagementRecord(TEST_MANAGEMENT_APP.appId, { status: 'running' });
-      btnRunTest.disabled = true;
-      setTimeout(()=> {
+      setTimeout(() => {
         location.reload();
       }, 4000);
       const info = new Notification({
@@ -43,5 +59,43 @@ const handleTestExecution = async (octokit: Octokit, event: any) => {
     spinner.close();
   });
   // @ts-ignore
-  btnRunTestElm.appendChild(btnRunTest);
+  headerSpace.appendChild(ddlPlugin);
+  // @ts-ignore
+  headerSpace.appendChild(btnRunTest);
+};
+
+const handleTestExecution = async (octokit: Octokit, event: any) => {
+  const headerSpace = kintone.app.record.getHeaderMenuSpaceElement();
+  // const btnRunTestElm = document.getElementById(`user-js-${FIELD_CODES.btnCustomRun}`);
+  const btnRunTest = new Button({ text: 'Run test', type: 'submit' });
+  if (event.record.ddlTestStatus.value === 'running') {
+    btnRunTest.disabled = true;
+  }
+
+  btnRunTest.addEventListener('click', async () => {
+    const spinner = new Spinner({ text: 'Trigger test' });
+    spinner.open();
+    try {
+      const selectedPlugin = event.record.ddlPluginName.value;
+      await triggerPluginTestWorkflow(octokit, selectedPlugin);
+      await updateTestManagementRecord(TEST_MANAGEMENT_APP.appId, { status: 'running' });
+      btnRunTest.disabled = true;
+      setTimeout(() => {
+        location.reload();
+      }, 4000);
+      const info = new Notification({
+        text: 'Github Workflow has been triggered for testing. Please reload page after a couple minutes', type: 'info'
+      });
+      info.open();
+    } catch (e) {
+      const errorMsg = new Notification({
+        // @ts-ignore
+        text: `An error occurred while triggering workflow job!\n[${e.message}]`, type: 'danger'
+      });
+      errorMsg.open();
+    }
+    spinner.close();
+  });
+  // @ts-ignore
+  headerSpace.appendChild(btnRunTest);
 };
